4a13e1f177ef80bc8d888aa2babd69dc034bd01e
pwr220000 <pwr220000@utdallas.edu>
2025-06-29 20:28:14 -0500

    added prediction token

--- model.py
+++ model.py
@@ -12,38 +12,24 @@ from sklearn.metrics import accuracy_score
 # Optional: tqdm for progress bars
 from tqdm import tqdm
 
+from vocab_initializer import generateToken
+
 
 
 class MultiPerspectiveNN(nn.Module):
-  def __init__(self, input_dim, hidden_dims, output_dim):
+  def __init__(self, input_size, hidden_size, num_layers):
     super().__init__()
-
-    layers = [] # array for each layer
-    current_dim = input_dim
-
-    for h_dim in hidden_dims: # simple appends each layer based on the dimensions 
-      layers.append(nn.Linear(current_dim, h_dim)) 
-      layers.append(nn.ReLU())
-      current_dim = h_dim
-
-    # layers.append(nn.Linear(current_dim,output_dim))
-    # layers.append(nn.Sigmoid()) 
-
-    init_weight = lambda *shape: nn.Parameter(torch.randn(*shape) * sigma)
-    triple = lambda: (init_weight(num_inputs, num_hiddens),
-                      init_weight(num_hiddens, num_hiddens),
-                      nn.Parameter(torch.zeros(num_hiddens)))
     
-    
-    self.W_xi, self.W_hi, self.b_i = triple()  # Input gate
-    self.W_xf, self.W_hf, self.b_f = triple()  # Forget gate
-    self.W_xo, self.W_ho, self.b_o = triple()  # Output gate
-    self.W_xc, self.W_hc, self.b_c = triple()  # Input node
+    self.lstm = nn.LSTM(input_size, hidden_size, num_layers, bias=True)
+  
 
-    self.model = nn.Sequential(*layers)
+  def forward(self, inputs, input_size, seq_len, H_C): # this is going to be a trained decoder
 
+    # the forward function here serves to take the last hidden state from the BERT encoder 
+    # use this hidden state, and given a correct_text, generate text with until it hits the same length
+    # at each iteration or word being generated, use the current hidden state to generate the next predicted word 
+    # loss gets evaluated at the end of the entire sentence, and this model only returns the predicted words with the specified length
 
-  def forward(self, inputs, H_C): # this is going to be a train decoder
     if H_C is None: # no hidden state or previous memory to use, new model
         # Initial state with shape: (batch_size, num_hiddens)
         H = torch.zeros((inputs.shape[1], self.num_hiddens),
@@ -51,24 +37,21 @@ class MultiPerspectiveNN(nn.Module):
         C = torch.zeros((inputs.shape[1], self.num_hiddens),
                       device=inputs.device)
     else:
-        H, C = H_C # if there is a hidden state, use the same memory 
+        H, C = H_C 
+        # if there is a hidden state, use the same memory 
     outputs = []
-
-    for X in inputs: # inputs is a token array, each token is passed through each gate
-        I = torch.sigmoid(torch.matmul(X, self.W_xi) +
-                        torch.matmul(H, self.W_hi) + self.b_i)
-        F = torch.sigmoid(torch.matmul(X, self.W_xf) +
-                        torch.matmul(H, self.W_hf) + self.b_f)
-        O = torch.sigmoid(torch.matmul(X, self.W_xo) +
-                        torch.matmul(H, self.W_ho) + self.b_o)
-        C_tilde = torch.tanh(torch.matmul(X, self.W_xc) +
-                           torch.matmul(H, self.W_hc) + self.b_c)
-        C = F * C + I * C_tilde
-        H = O * torch.tanh(C)
-        outputs.append(H)
-
-
-    return outputs, (H,C)
+    # outputs is the array of all hidden states
+    sentences = []
+    for i in range(seq_len):
+        # inputs is the last hidden state of the BERT encoder
+        # lstm takes in (seq, features) 
+        output, (H, C) = self.lstm((seq_len, input_size), (H, C))
+        # pass in the hidden and cell state through the LSTM
+        predicted_word = generateToken(H)
+        sentences.append(predicted_word)
+        
+        
+    return outputs, (H,C), sentences
   
 
   
--- vocab_initializer.py
+++ vocab_initializer.py
@@ -1,13 +1,41 @@
-from transformers import AutoTokenizer, BertForPreTraining
+from transformers import AutoTokenizer, TFBertModel
 import torch
+import torch.nn as nn
+
+from model import MultiPerspectiveNN
 
 tokenizer = AutoTokenizer.from_pretrained("google-bert/bert-base-uncased")
-model = BertForPreTraining.from_pretrained("google-bert/bert-base-uncased")
+model = TFBertModel.from_pretrained("google-bert/bert-base-uncased")
+
+input_dim = 768
+hidden_size = 512
+sigma = 0.01
+num_layers = 2
+
+vocab_size = 30522
+
+loss_function = nn.crossEntropyLoss()
+# compares the correct token with the predicted token
+
+linear_layer = nn.Linear(input_dim, vocab_size) 
+# turns the raw hidden state into a logits for each token, hence the size of the input(BERT embeddings) and the output(vocab size)
+
+lstm = nn.LSTM(input_dim, hidden_size, num_layers, bias=True)
+# LSTM used to generate text based on the output length inside of the training data
 
 inputs = tokenizer("Hello, my dog is cute", return_tensors="pt") 
 # using a pre-trained tokenizer because this reduces the stress of creating my own tokenizer and vocabulary 
-outputs = model(**inputs) 
-# the outputs a
 
-prediction_logits = outputs.prediction_logits # this will be the explanation with the correct logits used for training 
-seq_relationship_logits = outputs.seq_relationship_logits
+outputs = model(inputs) 
+# running the inputs through the model
+
+hidden_embeddings = outputs.last_hidden_state 
+# gets the last hidden state, which will be used as input for the decoder for text generation
+
+hidden_states = decoder(hidden_embeddings)
+# hidden states that will be used to evaluate prediction accuracy 
+
+def generateToken(hidden_state): 
+  layer = linear_layer(hidden_state)
+  predicted_word = nn.softMax(layer)
+  return predicted_word
